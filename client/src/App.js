0799f336b4df79e1de8b3ebde0e2e8f3a6dfe9f7
Fazal Mahmood <fazal_mahmood@live.com>
2020-04-01 03:05:22 -0500

    finished practice of graphQL

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+import Paper from "@material-ui/core/Paper";
+
+const UserQuery = gql`
+    {
+        users {
+            id
+            firstName
+            lastName
+        }
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, error, data } = useQuery(UserQuery);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.users.map(({ id, firstName, lastName }) => (
+        <div key={id}>
+            <p>
+                {id}: {firstName} {lastName}
+            </p>
+        </div>
+    ));
 }
 
 export default App;
